9af325d305f518f0854c42d77c6dc79b343a6675
Hyeoni18 <gusgnl105@gmail.com>
2022-03-04 19:04:56 +0900

    PasswordEncoder

--- src/main/java/hello/springboot/springsecurity/account/AccountService.java
+++ src/main/java/hello/springboot/springsecurity/account/AccountService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,8 @@ public class AccountService implements UserDetailsService {
     // Spring Security가 요구하는 패턴. {noop}에는 알고리즘 이름이 들어감. 패스워드를 인코딩하는.
     // TODO {noop}123
     @Autowired AccountRepository accountRepository;
+
+    @Autowired PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -31,7 +34,7 @@ public class AccountService implements UserDetailsService {
     public Account createNew(Account account) {
         //account.setPassword("{noop}"+account.getPassword()); //패스워드 인코딩
         //account 안으로 넣을 수도 있음.
-        account.encodePassword();
+        account.encodePassword(passwordEncoder);
         return accountRepository.save(account);
     }
 }
