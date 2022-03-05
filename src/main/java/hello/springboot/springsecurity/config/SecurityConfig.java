d24c3ac89898cf8cfda5631aa7cd5808abddda16
Hyeoni18 <gusgnl105@gmail.com>
2022-03-06 03:12:26 +0900

    로그아웃 처리 필터: LogoutFilter

--- src/main/java/hello/springboot/springsecurity/config/SecurityConfig.java
+++ src/main/java/hello/springboot/springsecurity/config/SecurityConfig.java
@@ -51,6 +51,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout()
+                .logoutUrl("/logout") //로그아웃 url설정. 나중에 커스텀할 때 화면 만들고 변경하면 돼.
+                .logoutSuccessUrl("/") //기본은 /login
+        //        .addLogoutHandler() //로그아웃할 때 부가적인 일을 추가할 수 있음.
+        //        .logoutSuccessUrl() //별도로 구현해서 넣으면 돼.
+        //        .invalidateHttpSession(true) //로그아웃 한 다음 세션을 invalid 처리할거냐. 기본값은 true
+        //        .deleteCookies("") //만약 쿠키 기반의 로그인을 사용했다. 그러면 쿠키의 이름을 적어줘. 나중에 새로운 쿠키를 발급받게 해.
+                ;
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
